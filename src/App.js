67f3d9c4370d745e8067692de5e11f9d7a68f6c4
Philipe <liperapltda@gmail.com>
2022-10-26 19:20:10 -0300

    exercicio 1 commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lista de Compras</h1>
+      <ul>
+        <li>Pão</li>
+        <li>Banana</li>
+        <li>Milk Shake de Doce de Leite</li>
+        <li>Nutella</li>
+        <li>Sorvete</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
